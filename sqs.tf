ba67e071aea0db63fd7891aace9ff9cefb381113
DhruvJWork <e-dvph@GFT.com>
2022-07-12 14:53:55 +0100

    sqs terraform changes

--- sqs.tf
+++ sqs.tf
@@ -1,8 +1,42 @@
 data "aws_caller_identity" "current" {}
+
+locals {
+    account_id = data.aws_caller_identity.current.account_id
+}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+##
+## A module to create an SQS queue, along with its dead-letter queue and access policies
+##
+
+#provider "aws" {}
+
+data "aws_region" "current" {}
+
+##
+## The primary and dead-letter queues
+##
+
+resource "aws_sqs_queue" "base_queue" {
+  name                        = var.queue_name
+  message_retention_seconds   = var.retention_period
+  visibility_timeout_seconds  = var.visibility_timeout
+  redrive_policy              = jsonencode({
+                                    "deadLetterTargetArn" = aws_sqs_queue.deadletter_queue.arn,
+                                    "maxReceiveCount" = var.receive_count
+                                })
+}
+
+resource "aws_sqs_queue" "deadletter_queue" {
+  name                        = "${var.queue_name}-DLQ"
+  message_retention_seconds   = var.retention_period
+  visibility_timeout_seconds  = var.visibility_timeout
+}
+
 resource "aws_sqs_queue_policy" "test" {
-  queue_url = aws_sqs_queue.q.id
-
-
+  queue_url = aws_sqs_queue.base_queue.id
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -20,9 +54,59 @@ resource "aws_sqs_queue_policy" "test" {
           ] 
         }, 
       "Action": "sqs:*",       
-      "Resource": "arn:aws:sqs:us-east-1:"${data.aws_caller_identity.current}":LambdaCrossAccountQueue"     
+      "Resource": "arn:aws:sqs:us-east-1:"${local.account_id}":LambdaCrossAccountQueue"     
     }   
   ]
 } 
 POLICY
+}
+
+##
+## Managed policies that allow access to the queue
+##
+
+resource "aws_iam_policy" "consumer_policy" {
+  name        = "SQS-${var.queue_name}-${data.aws_region.current.name}-consumer_policy"
+  description = "Attach this policy to consumers of ${var.queue_name} SQS queue"
+  policy      = data.aws_iam_policy_document.consumer_policy.json
+}
+
+data "aws_iam_policy_document" "consumer_policy" {
+  statement {
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:ChangeMessageVisibilityBatch",
+      "sqs:DeleteMessage",
+      "sqs:DeleteMessageBatch",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.base_queue.arn,
+      aws_sqs_queue.deadletter_queue.arn
+    ]
+  }
+}
+
+
+resource "aws_iam_policy" "producer_policy" {
+  name        = "SQS-${var.queue_name}-${data.aws_region.current.name}-producer"
+  description = "Attach this policy to producers for ${var.queue_name} SQS queue"
+  policy      = data.aws_iam_policy_document.producer_policy.json
+}
+
+data "aws_iam_policy_document" "producer_policy" {
+  statement {
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage",
+      "sqs:SendMessageBatch",
+      "lambda.*"
+    ]
+    resources = [
+      aws_sqs_queue.base_queue.arn
+    ]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -37,3 +37,32 @@ variable "install_date" {
 variable "destintion_account"{
     type = string
 }
+
+#####SQS Variables
+
+variable "queue_name" {
+  description = "The name of the queue. Used as a prefix for related resource names."
+  type = string
+  default = "report-delivery-queue"
+}
+
+
+variable "retention_period" {
+  description = "Time (in seconds) that messages will remain in queue before being purged"
+  type = number
+  default = 86400
+}
+
+
+variable "visibility_timeout" {
+  description = "Time (in seconds) that consumers have to process a message before it becomes available again"
+  type = number
+  default = 60
+}
+
+
+variable "receive_count" {
+  description = "The number of times that a message can be retrieved before being moved to the dead-letter queue"
+  type = number
+  default = 3
+}
